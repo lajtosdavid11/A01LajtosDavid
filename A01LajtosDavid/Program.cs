179baf32ad70b2594d2f695e194bace8f7dcb6f2
lajtosdavid11 <lajtos.david11@gmail.com>
2020-09-14 10:57:13 +0200

    Beolvasás

--- A01LajtosDavid/Program.cs
+++ A01LajtosDavid/Program.cs
@@ -3,13 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace A01LajtosDavid
 {
+    
     class Program
     {
+        static int oszlop = 1000;
+        static int[] tomb = new int[oszlop];
+
+        static void beol()
+        {
+            StreamReader sr = new StreamReader("adatok.dat");
+            string[] atmeneti;
+            int i = 0;
+            while (!sr.EndOfStream)
+            {
+                atmeneti = Console.ReadLine().Split();
+                for (int j = 0; j < oszlop; j++)
+                {
+                    tomb[j] = Convert.ToInt32(atmeneti[j]);
+                }
+                i++;
+            }
+            sr.Close();
+        }
+
         static void Main(string[] args)
         {
+            beol();
+
 
         }
     }
